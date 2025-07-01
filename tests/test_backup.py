719dea4e1ce43108e0dec62d73698daab58cbde1
Raphael Gabbarelli <raphael@gabbarelli.it>
2025-07-01 07:06:29 +0200

    abandoning monkeypatch

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -16,12 +16,12 @@ class MockPath(Path):
 
 
 @pytest.fixture
-def fake_paths_to_pictures(monkeypatch) -> list[Path]:
+def fake_paths_to_pictures(mocker) -> list[Path]:
      
-        monkeypatch.setattr(file_one := MockPath('/fake/path/to/source/picture_1.heic'), 'read_bytes', lambda: b'Pieces of what we could have been')
-        monkeypatch.setattr(file_two := MockPath('/fake/path/to/source/picture_2.gif'), 'read_bytes', lambda: b'Pieces of a shattered dream')
-        monkeypatch.setattr(file_three := MockPath('/fake/path/to/source/picture_3.jpeg'), 'read_bytes', lambda: b'Child, take your dark memories')
-        monkeypatch.setattr(file_four := MockPath('/fake/path/to/source/picture_4.png'), 'read_bytes', lambda: b'Like seeds and plant them far from here')
+        mocker.patch.object(file_one := MockPath('/fake/path/to/source/picture_1.heic'), 'read_bytes', lambda: b'Pieces of what we could have been')
+        mocker.patch.object(file_two := MockPath('/fake/path/to/source/picture_2.gif'), 'read_bytes', lambda: b'Pieces of a shattered dream')
+        mocker.patch.object(file_three := MockPath('/fake/path/to/source/picture_3.jpeg'), 'read_bytes', lambda: b'Child, take your dark memories')
+        mocker.patch.object(file_four := MockPath('/fake/path/to/source/picture_4.png'), 'read_bytes', lambda: b'Like seeds and plant them far from here')
         return [file_one, file_two, file_three, file_four]
 
 def test_traverse_source(mocker, fake_paths_to_pictures):
